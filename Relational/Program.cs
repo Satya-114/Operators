5f57e29604bed03155737ef7e9b81d7a4d67a590
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:10:16 +0530

    Add files via upload

---
+++ Relational/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Relational_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			bool result;
+			int firstNumber = 10, secondNumber = 20;
+
+			result = (firstNumber == secondNumber);
+			Console.WriteLine("{0} == {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber > secondNumber);
+			Console.WriteLine("{0} > {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber < secondNumber);
+			Console.WriteLine("{0} < {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber >= secondNumber);
+			Console.WriteLine("{0} >= {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber <= secondNumber);
+			Console.WriteLine("{0} <= {1} returns {2}", firstNumber, secondNumber, result);
+
+			result = (firstNumber != secondNumber);
+			Console.WriteLine("{0} != {1} returns {2}", firstNumber, secondNumber, result);
+		}
+	}
+}
+        
+    
+
