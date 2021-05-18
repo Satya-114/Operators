bb8143ce2ba75265a414ec5fec04e663de7c90dc
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:07:05 +0530

    Add files via upload

---
+++ Arithmetic/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+	            double firstNumber = 14, secondNumber = 4, result;
+				int num1 = 26, num2 = 4, rem;
+
+			
+				result = firstNumber + secondNumber;
+				Console.WriteLine("{0} + {1} = {2}", firstNumber, secondNumber, result);
+
+				
+				result = firstNumber - secondNumber;
+				Console.WriteLine("{0} - {1} = {2}", firstNumber, secondNumber, result);
+
+				
+				result = firstNumber * secondNumber;
+				Console.WriteLine("{0} * {1} = {2}", firstNumber, secondNumber, result);
+
+				
+				result = firstNumber / secondNumber;
+				Console.WriteLine("{0} / {1} = {2}", firstNumber, secondNumber, result);
+
+				
+				rem = num1 % num2;
+				Console.WriteLine("{0} % {1} = {2}", num1, num2, rem);
+			}
+		}
+	}
+				
+
