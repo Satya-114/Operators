c7e8dd0551b6ae8fff39734fa8cb49ebef95f3bb
Satya-114 <83812540+Satya-114@users.noreply.github.com>
2021-05-18 23:15:15 +0530

    Add files via upload

---
+++ Logical/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Logical_operators
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+			bool result;
+			int firstNumber = 10, secondNumber = 20;
+
+			
+			result = (firstNumber == secondNumber) || (firstNumber > 5);
+			Console.WriteLine(result);
+
+		
+			result = (firstNumber == secondNumber) && (firstNumber > 5);
+			Console.WriteLine(result);
+		}
+    }
+}
